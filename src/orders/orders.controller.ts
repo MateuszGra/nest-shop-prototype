a6591f27dc81465d715d645f0f841c64966eff8a
Mateusz Grabowski <m.grabowski@rolladengroup.de>
2021-01-14 18:33:04 +0100

    orders module

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Inject } from '@nestjs/common';
+import { OrdersService } from "./orders.service";
+
+@Controller('orders')
+export class OrdersController {
+    constructor(
+        @Inject(OrdersService) private ordersService: OrdersService,
+    ) {
+
+    }
+}
