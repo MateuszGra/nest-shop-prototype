1f3ac904f592ed6aca9fa20d91cfcd6a49c21bb3
Mateusz Grabowski <m.grabowski@rolladengroup.de>
2021-01-14 19:02:38 +0100

    show one by order number method

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Inject } from '@nestjs/common';
+import {Controller, Get, Inject, Param} from '@nestjs/common';
 import { OrdersService } from "./orders.service";
+import {OrderResp} from "../interfaces/orders";
 
 @Controller('orders')
 export class OrdersController {
     constructor(
         @Inject(OrdersService) private ordersService: OrdersService,
     ) {
-
     }
+
+    @Get('/:orderNumber')
+    async showOneByOrderNumber(
+        @Param('orderNumber') orderNumber: string,
+    ): Promise<OrderResp> {
+        return await this.ordersService.getOneByOrderNumber(orderNumber);
+    }
+
+
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { OrderResp } from "../interfaces/orders";
+import { ResponseStatus } from "../interfaces/response-status";
+import { OrdersEntity } from "./orders.entity";
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    async getOneByOrderNumber(orderNumber): Promise<OrderResp> {
+        const [order, count]: [OrdersEntity[], number] = await OrdersEntity.findAndCount({
+            where: {
+                orderNumber
+            }
+        });
+        if (order.length > 0) {
+            return {
+                isSuccess: true,
+                status: ResponseStatus.ok,
+                count: count,
+                order: order,
+            }
+        } else {
+            return {
+                isSuccess: false,
+                status: ResponseStatus.ok,
+                errors: ["Empty"],
+            }
+        }
+    }
+}
